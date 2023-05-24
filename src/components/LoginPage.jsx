f3db21acedd1b53759a245d4b4b62c8eed014a63
tarik350 <80385670+tarik350@users.noreply.github.com>
2023-05-24 22:31:18 +0300

    login error display done

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,13 @@
 import { useState, useRef, useEffect } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 
-import { gql, useQuery } from "@apollo/client";
+import { NetworkStatus, gql, useLazyQuery, useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import useDidMountEffect from "./utils/useDidMountEffect";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { pharma_woman } from "../assets";
+import { RiContactsBookLine } from "react-icons/ri";
+import { selectionSetMatchesResult } from "@apollo/client/cache/inmemory/helpers";
 
 const login = gql`
   query myQuery($email: String!, $password: String!) {
@@ -17,75 +19,64 @@ const login = gql`
 `;
 
 const LoginPage = () => {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
   let navigate = useNavigate();
 
-  const [email, setEmail] = useState(null);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   ///the problem here is the query runs every time the component mounts
   ///i only want the query to excute when i click the click button i thout i have implemented that
-  const { data, error, loading, refetch } = useQuery(login, {
-    enabled: false,
-    variables: {
-      email,
-      password,
-    },
-  });
 
   const [passwordVisibility, setPassordVisibility] = useState("password");
 
-  useDidMountEffect(() => {
-    console.log(`email setted to : ${email}`);
+  // console.log(loading);
 
-    refetch()
-      .then((value) => {
-        console.log(`refetch running`);
-        console.log(`value retrned from backend: ${value.data}`);
+  // const { data, error, loading, refetch, networkStatus } = useQuery(login, {
+  //   // enabled: false,
+  //   notifyOnNetworkStatusChange: true,
+  //   variables: {
+  //     email,
+  //     password,
+  //   },
+  // });
 
-        if (value.data) {
-          console.log(`user has data: ${value.data}`);
-
-          //if there is a data
-          localStorage.setItem("token", value.data.login.token);
-          localStorage.setItem("id", value.data.login.id);
-
-          navigateHome();
-        } else {
-          //if the user not found
-
-          const errorMessage = value.errors[0].message;
-          console.log(`erorr from auth: ${errorMessage}`);
-        }
-      })
-      .catch((erorr) => {
-        //user not found
-        console.log(`erorr from apollo: ${erorr}`);
-      });
-  }, [email, password]);
-
-  const navigateHome = () => {
-    // 👇️ navigate to /
-    navigate("/");
-  };
+  const [
+    getLoginData,
+    { loading: lazyLoading, error: lazyError, data: lasyData },
+  ] = useLazyQuery(login);
 
   const handleLogin = async () => {
-    if (error) {
-    }
-    //add validation to check if the email and password value are
-    //diffrent from null
     console.log("login running ");
 
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-
     if (email && password) {
-      setEmail(email);
-      setPassword(password);
+      getLoginData({
+        variables: {
+          email,
+          password,
+        },
+      })
+        .then((value) => {
+          if (value.data) {
+            console.log(`user has data: ${value.data}`);
+
+            //if there is a data
+            localStorage.setItem("token", value.data.login.token);
+            localStorage.setItem("id", value.data.login.id);
+
+            // navigateHome();
+            navigate("/");
+          } else {
+            //if the user not found
+
+            const errorMessage = value.errors[0].message;
+            console.log(`erorr from auth: ${errorMessage}`);
+          }
+        })
+        .catch((err) => {
+          console.log(`error is : ${err}`);
+        });
     } else {
-      //validation
-      //make the border red and show error massage
+      console.log("email and password can not be empty");
     }
   };
 
@@ -111,14 +102,20 @@ const LoginPage = () => {
           </div>
           <div className="flex  bg-white flex-col w-[500px]">
             <input
-              ref={emailRef}
+              // ref={emailRef}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="text"
               className="rounded-md w-full text-[16px] grow  border focus:outline-none focus:border-primary  border-gray-400  px-3 py-[20px]"
               placeholder="email"
             />
             <div className="flex-1 relative">
               <input
-                ref={passwordRef}
+                // ref={passwordRef}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
                 type={passwordVisibility}
                 className=" rounded-md my-4 w-full text-[16px] grow  border focus:outline-none focus:border-primary  border-gray-400  px-3 py-[20px] "
                 placeholder="password"
@@ -136,14 +133,19 @@ const LoginPage = () => {
                 )}
               </button>
             </div>
-            {/* <div className="massage">{error.message}</div> */}
+            <div className="error massage">
+              {lazyError ? lazyError.message : ""}
+            </div>
             <div className="flex justify-between">
               <button
                 className="mr-[10px] flex justify-center  uppercase py-[20px] rounded-xl flex-1  text-white bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500"
                 title="Login"
-                onClick={handleLogin}
+                onClick={() => {
+                  handleLogin();
+                }}
               >
-                {loading ? (
+                {/* login */}
+                {lazyLoading ? (
                   <CircularProgress size="1rem" style={{ color: "white" }} />
                 ) : (
                   "login"
