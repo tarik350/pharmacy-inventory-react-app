41d24ff427968f8202e0f4ea01eddaa76ae2349f
tarik350 <80385670+tarik350@users.noreply.github.com>
2023-06-11 23:00:31 +0300

    privacy policy done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,29 @@ import Auth from "./Auth";
 import Card from "./utils/Card";
 import { useLocation, useNavigate } from "react-router-dom";
 
+import { gql, useQuery } from "@apollo/client";
+
+const GETUSER = gql`
+  query getUser($uid: uuid!) {
+    users(where: { id: { _eq: $uid } }) {
+      email
+      name
+      pharmacy_name
+      phone_number
+      users_locations {
+        address
+      }
+    }
+  }
+`;
 const Home = () => {
   const autorized = localStorage.getItem("token");
   const navigate = useNavigate();
+  const uid = localStorage.getItem("id");
+
+  const { data, error, loading } = useQuery(GETUSER, {
+    variables: { uid },
+  });
 
   useEffect(() => {
     console.log(`autorized: ${autorized} `);
@@ -17,52 +37,60 @@ const Home = () => {
     }
   }, []);
 
+  data && console.log(`data : ${data}`);
+  error && console.log(`error: ${error}`);
+
+  if (loading) return <div>loading..</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <main className="flex flex-col mx-[40px] justify-center items-center  ">
-      <div className="self-end my-8">{/* <Auth /> */}</div>
-      <section className="flex  justify-center items-center flex-2 ">
-        <div className="w-1/2 grow">
-          <div className="bg-black w-max text-white px-4 rounded-full py-[3px]  font-extralight ">
-            welcome
-          </div>
-          <p className="text-[80px] font-poppins  font-bold leading-[130%] bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text inline-block text-transparent">
-            Manage your Pharmacy with Samaritan
-          </p>
-
-          <p className=" leading-[150%] my-8 text-gray-500">
-            is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s, when an unknown printer took a galley of type and scrambled
-            it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more
-            recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum{" "}
-          </p>
-
-          <div className="flex items-center">
-            <button className="bg-black text-white px-6 py-4 mr-6">
-              Get Started
-            </button>
-            <p>
-              not just an inventory system,{" "}
-              <span className=" block font-poppins  font-bold leading-[130%] bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text  text-transparent">
-                1 up for your buisness
-              </span>
+    data && (
+      <main className="flex flex-col mx-[40px] justify-center items-center  ">
+        <div className="self-end my-8">{/* <Auth /> */}</div>
+        <section className="flex  justify-center items-center flex-2 ">
+          <div className="w-1/2 grow">
+            <div className="bg-black w-max text-white px-4 rounded-full py-[3px]  font-extralight ">
+              welcome
+            </div>
+            <p className="text-[80px] font-poppins  font-bold leading-[130%] bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text inline-block text-transparent">
+              Manage your Pharmacy with Samaritan
             </p>
+
+            <p className=" leading-[150%] my-8 text-gray-500">
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised
+              in the 1960s with the release of Letraset sheets containing Lorem
+              Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum{" "}
+            </p>
+
+            <div className="flex items-center">
+              <button className="bg-black text-white px-6 py-4 mr-6">
+                Get Started
+              </button>
+              <p>
+                not just an inventory system,{" "}
+                <span className=" block font-poppins  font-bold leading-[130%] bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 bg-clip-text  text-transparent">
+                  1 up for your buisness
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="ml-24 w-[500px] h-[500px] ">
-          <img
-            className=" object-contain  "
-            src={pharma_woman}
-            alt="pharmcist woman"
-          />
-        </div>
-        {/* for some rectangular card for the ui */}
-      </section>
-    </main>
+          <div className="ml-24 w-[500px] h-[500px] ">
+            <img
+              className=" object-contain  "
+              src={pharma_woman}
+              alt="pharmcist woman"
+            />
+          </div>
+          {/* for some rectangular card for the ui */}
+        </section>
+      </main>
+    )
   );
 };
 
--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -2,8 +2,327 @@ import React from "react";
 
 const PrivacyPolicy = () => {
   return (
-    <div className="text-[120px] flex justify-center items-center h-screen">
-      Privacy policy
+    <div className="flex mx-[40px] justify-center items-center my-12    ">
+      <div className="flex flex-1  rounded-xl  bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500">
+        <div className=" flex-col relative  bg-white w-full   m-[3px] border-2 p-12  shadow-2xl rounded-lg ">
+          <h1 className="heading-one">Privacy Policy</h1>
+          <div>
+            <h2 className="heading-two">Introduction.</h2>
+            <p className="paragraph">
+              Are you running a pharmacy and looking for a reliable web
+              application to manage your inventory? Look no further than
+              Samaritan! In today's digital age, safeguarding the privacy of
+              sensitive information is of utmost importance. This article will
+              provide you with a comprehensive overview of the privacy policy
+              section for Samaritan, ensuring that your data remains secure and
+              your pharmacy inventory management runs smoothly. Let's delve into
+              the essential guidelines that Samaritan adheres to, giving you
+              peace of mind and allowing you to focus on what matters most –
+              providing exceptional healthcare services to your community.Are
+              you running a pharmacy and looking for a reliable web application
+              to manage your inventory? Look no further than Samaritan! In
+              today's digital age, safeguarding the privacy of sensitive
+              information is of utmost importance. This article will provide you
+              with a comprehensive overview of the privacy policy section for
+              Samaritan, ensuring that your data remains secure and your
+              pharmacy inventory management runs smoothly. Let's delve into the
+              essential guidelines that Samaritan adheres to, giving you peace
+              of mind and allowing you to focus on what matters most – providing
+              exceptional healthcare services to your community.
+            </p>
+          </div>
+          <section>
+            <h3 className="heading-three">
+              section 1: Introduction to Samaritan's Privacy Policy
+            </h3>
+            <p className="paragraph">
+              At Samaritan, we understand the criticality of protecting your
+              privacy and the confidentiality of your pharmacy inventory data.
+              Our privacy policy aims to outline the measures we take to
+              safeguard your information and maintain the highest standards of
+              data security. By using the Samaritan web application, you entrust
+              us with valuable data, and we honor that trust by implementing
+              robust privacy practices. Below, we will explore the key aspects
+              of our privacy policy, ensuring transparency and accountability in
+              handling your data.
+            </p>
+            <div className="secton 2">
+              <h3 className="heading-three">
+                Section 2: Data Collection and Usage
+              </h3>
+              <div className="section 2.1">
+                <h4 className="heading-four">
+                  2.1 Collection of Personal Information
+                </h4>
+                <p className="paragraph">
+                  When you sign up for Samaritan, we collect essential personal
+                  information such as your name, email address, and contact
+                  details. This information allows us to provide you with access
+                  to our web application and personalize your experience. We
+                  assure you that any personal information collected is used
+                  solely for the purpose of delivering our services and
+                  enhancing your user experience.
+                </p>
+              </div>
+              <div className="section 2.2">
+                <h4 className="heading-four">
+                  2.2 Use of Non-Personal Information
+                </h4>
+                <p className="paragraph">
+                  In addition to personal information, Samaritan may also
+                  collect non-personal information, including device
+                  information, IP addresses, and browser details. This
+                  information helps us optimize our web application, improve
+                  user experience, and provide valuable insights for future
+                  enhancements. Rest assured, any non-personal information
+                  collected is anonymized and used in an aggregated manner to
+                  maintain your privacy.
+                </p>
+              </div>
+              <div className="section 2.3">
+                <h4 className="heading-four">
+                  2.3 Cookies and Tracking Technologies
+                </h4>
+                <p className="paragraph">
+                  Samaritan utilizes cookies and similar tracking technologies
+                  to enhance your browsing experience and gather relevant
+                  information. These technologies enable us to remember your
+                  preferences, analyze usage patterns, and provide personalized
+                  content. You have the option to accept or decline cookies
+                  through your browser settings, but please note that certain
+                  features of Samaritan may be affected if you choose to disable
+                  cookies.
+                </p>
+              </div>
+            </div>
+            <div className="section 3">
+              <h3 className="heading-three">
+                Section 3: Data Security and Confidentiality Measures
+              </h3>
+              <div className="section 3.1">
+                <h4 className="heading-four">
+                  3.1 Encryption and Secure Communication
+                </h4>
+                <p className="paragraph">
+                  We prioritize the security of your data and employ
+                  industry-standard encryption protocols to ensure secure
+                  transmission of information between your device and our
+                  servers. This ensures that your pharmacy inventory data
+                  remains confidential and protected from unauthorized access.
+                </p>
+              </div>
+              <div className="section 3.2">
+                <h4 className="heading-four">
+                  3.2 Access Controls and Authentication
+                </h4>
+                <p className="paragraph">
+                  Samaritan implements strict access controls and authentication
+                  mechanisms to ensure that only authorized individuals have
+                  access to your data. User accounts are protected by unique
+                  login credentials, and you are encouraged to use strong
+                  passwords to further enhance security.
+                </p>
+              </div>
+              <div className="section 3.3">
+                <h4 className="heading-four">3.3 Regular Data Backups</h4>
+                <p className="paragraph">
+                  To mitigate the risk of data loss, Samaritan maintains regular
+                  backups of your pharmacy inventory data. These backups are
+                  securely stored and can be restored in the event of any
+                  unforeseen circumstances. Rest assured, we have contingency
+                  plans in place to minimize disruption and ensure business
+                  continuity.
+                </p>
+              </div>
+              <div className="section 3.4">
+                <h4 className="heading-four">
+                  3.4 Compliance with Data Protection Regulations
+                </h4>
+                <p className="paragraph">
+                  Samaritan is committed to adhering to relevant data protection
+                  regulations, including but not limited to the General Data
+                  Protection Regulation (GDPR) and the Health Insurance
+                  Portability and Accountability Act (HIPAA). We maintain strict
+                  data protection practices and ensure that your data is
+                  processed lawfully, fairly, and transparently.
+                </p>
+              </div>
+            </div>
+            <div className="section 4">
+              <h3 className="heading-three">
+                Section 4: Third-Party Services and Data Sharing
+              </h3>
+              <div className="section 4.1">
+                <h4 className="heading-four">4.1 Third-Party Integrations</h4>
+                <p className="paragraph">
+                  Samaritan may integrate with third-party services to enhance
+                  the functionality of our web application. However, we
+                  carefully vet these service providers to ensure they uphold
+                  the same level of privacy and security standards as Samaritan.
+                  We will never share your data with any third party without
+                  your explicit consent.
+                </p>
+              </div>{" "}
+              <div className="section 4.2">
+                <h4 className="heading-four">
+                  4.2 Legal Disclosure and Compliance
+                </h4>
+                <p className="paragraph">
+                  In certain circumstances, Samaritan may be obligated to
+                  disclose your personal information in response to lawful
+                  requests, court orders, or legal proceedings. Rest assured, we
+                  will only disclose information to the extent required by law
+                  and will notify you of such instances, where possible and
+                  legally permissible.
+                </p>
+              </div>
+              <div className="section 4.3">
+                <h4 className="heading-four">
+                  4.3 Data Transfer and International Operations
+                </h4>
+                <p className="paragraph">
+                  As Samaritan operates globally, your data may be stored or
+                  processed in countries outside your own. We take appropriate
+                  measures to ensure that any cross-border data transfers comply
+                  with applicable data protection laws and guarantee the same
+                  level of protection as outlined in this privacy policy.
+                </p>
+              </div>
+            </div>
+            <div className="section 5">
+              <h3 className="heading-three">
+                Section 5: Your Rights and Consent
+              </h3>
+              <div className="section 5.1">
+                <h4 className="heading-four">
+                  5.1 Right to Access and Rectify
+                </h4>
+                <p className="paragraph">
+                  You have the right to access and update your personal
+                  information stored within Samaritan. We provide you with the
+                  necessary tools and options to review, edit, or delete your
+                  data. If you encounter any difficulties, our support team is
+                  available to assist you.
+                </p>
+              </div>
+              <div className="section 5.2">
+                <h4 className="heading-four">5.2 Consent and Opt-Out</h4>
+                <p className="paragraph">
+                  By using Samaritan, you provide your consent to the
+                  collection, storage, and processing of your personal
+                  information as outlined in this privacy policy. You have the
+                  right to withdraw your consent at any time by discontinuing
+                  the use of Samaritan. Please note that certain services may no
+                  longer be available if you choose to opt-out.
+                </p>
+              </div>
+              <div className="section 5.3">
+                <h4 className="heading-four">5.3 Contact and Support</h4>
+                <p className="paragraph">
+                  If you have any questions, concerns, or inquiries regarding
+                  Samaritan's privacy policy or the security of your data, our
+                  dedicated support team is readily available to assist you. You
+                  can reach out to us through our contact channels, and we will
+                  respond promptly.
+                </p>
+              </div>
+            </div>
+            <div className="section 6">
+              <h3 className="heading-three">
+                Section 6: Best Practices for Secure Password Management
+              </h3>
+              <div className="section 5.3">
+                <h4 className="heading-four">
+                  6.1 Importance of Strong Passwords
+                </h4>
+                <p className="paragraph">
+                  One of the fundamental aspects of maintaining data security is
+                  ensuring the use of strong passwords. Weak or easily guessable
+                  passwords pose a significant risk to the confidentiality of
+                  your pharmacy inventory data. Samaritan encourages users to
+                  create passwords that are unique, complex, and not easily
+                  associated with personal information. By incorporating a
+                  combination of uppercase and lowercase letters, numbers, and
+                  special characters, you can significantly enhance the strength
+                  of your password.
+                </p>
+              </div>
+              <div className="section 5.3">
+                <h4 className="heading-four">
+                  6.2 Two-Factor Authentication (2FA)
+                </h4>
+                <p className="paragraph">
+                  Implementing two-factor authentication adds an extra layer of
+                  security to your Samaritan account. By requiring a second
+                  verification step, such as a unique code sent to your mobile
+                  device, 2FA ensures that only authorized individuals can
+                  access your account. Enabling this feature provides an
+                  additional safeguard against unauthorized access, even if your
+                  password is compromised.
+                </p>
+              </div>
+              <div className="section 5.3">
+                <h4 className="heading-four">6.3 Regular Password Updates</h4>
+                <p className="paragraph">
+                  To maintain the integrity of your account, it is essential to
+                  update your password regularly. By changing your password at
+                  least every three to six months, you minimize the risk of
+                  potential security breaches. Samaritan encourages users to
+                  adopt a proactive approach to password management and ensures
+                  a seamless password update process within the web application.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h3 className="heading-three">
+                Section 7: Protecting Your Pharmacy Inventory from Cyber Threats
+              </h3>
+              <div className="section 5.3">
+                <h4 className="heading-four">
+                  7.1 Recognizing Phishing Attempts
+                </h4>
+                <p className="paragraph">
+                  Phishing attacks are a common method used by cybercriminals to
+                  gain unauthorized access to sensitive information. Samaritan
+                  advises users to remain vigilant and cautious when interacting
+                  with emails, messages, or websites that appear suspicious or
+                  request personal information. By educating yourself and your
+                  team about common phishing techniques, you can reduce the risk
+                  of falling victim to these fraudulent activities.
+                </p>
+              </div>
+              <div className="section 5.3">
+                <h4 className="heading-four">
+                  7.2 Regular Security Updates and Patches
+                </h4>
+                <p className="paragraph">
+                  Samaritan prioritizes the security of its web application by
+                  regularly releasing updates and patches to address any
+                  identified vulnerabilities. Keeping your Samaritan instance up
+                  to date ensures that you benefit from the latest security
+                  enhancements and safeguards against emerging threats. It is
+                  crucial to promptly install updates and patches to maintain a
+                  secure environment for your pharmacy inventory management.
+                </p>
+              </div>
+              <div className="section 5.3">
+                <h4 className="heading-four">
+                  7.3 Employee Security Awareness Training
+                </h4>
+                <p className="paragraph">
+                  Human error is often a significant factor in data breaches and
+                  security incidents. Samaritan encourages pharmacy staff to
+                  undergo regular security awareness training to enhance their
+                  understanding of potential threats and the best practices to
+                  mitigate them. By fostering a culture of security awareness
+                  and education, you can significantly reduce the likelihood of
+                  data breaches caused by employee negligence.
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
